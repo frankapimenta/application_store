86f7f4d38e15f1fca0e6a5971cf613f34a02c5d3
111443 <frank.pimenta@swisslife-select.ch>
2019-02-27 12:13:45 +0100

    + Add TODO.

--- lib/application_store/config.rb
+++ lib/application_store/config.rb
@@ -2,6 +2,7 @@ module ApplicationStore
   class Config
 
     def initialize configuration_file_path = Config.default_configuration_file_path
+      # TODO: enable env via arguments
       @configuration_file_path = configuration_file_path
 
       raise StandardError.new "file does not exist or path given is wrong" unless File.exists?(@configuration_file_path)
