382b3ff9c9e2043ae380c62a49155f0e53a15b51
111443 <frank.pimenta@swisslife-select.ch>
2019-02-27 15:27:02 +0100

    + Set environment in configuration on initialization.

--- lib/application_store/config.rb
+++ lib/application_store/config.rb
@@ -1,8 +1,9 @@
 module ApplicationStore
   class Config
 
-    def initialize configuration_file_path = Config.default_configuration_file_path
+    def initialize environment: Config.environment,  configuration_file_path: Config.default_configuration_file_path
       # TODO: enable env via arguments
+      self.class.instance_variable_set(:@environment, environment)
       @configuration_file_path = configuration_file_path
 
       raise StandardError.new "file does not exist or path given is wrong" unless File.exists?(@configuration_file_path)
@@ -19,6 +20,7 @@ module ApplicationStore
           _environment
         end.to_sym
       end
+      # TODO ::configuration_file_path
       def config_path
         File.join(ApplicationStore::root_path, 'lib/config')
       end
--- spec/application_store/config_spec.rb
+++ spec/application_store/config_spec.rb
@@ -1,7 +1,20 @@
 RSpec.describe ApplicationStore::Config do
   let(:configuration_file_path)         {  ApplicationStore::Config.default_configuration_file_path }
   let(:full_path_to_configuration_file) { configuration_file_path }
-  context "initialization" do
+  context "on initialization" do
+    before { allow(ENV).to receive(:[]).with('APPLICATION_STORE_ENVIRONMENT').and_return 'development' }
+    context "on setting @environment" do
+      specify "sets environment from APP ENV when environment is not given" do
+        expect(described_class).to receive(:environment).and_return :development
+        described_class.new
+        expect(described_class.instance_variable_get(:@environment)).to eq :development
+      end
+      specify "sets given environment" do
+        described_class.new environment: :production
+        expect(described_class.instance_variable_get(:@environment)).to eq :production
+        expect(described_class.environment).to eq :production
+      end
+    end
     specify "initialized with no error when no configuration file path is given (using default configuration file)" do
       expect { described_class.new }.not_to raise_error
     end
