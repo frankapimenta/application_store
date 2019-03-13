c336c49b43749a4ae375e695c7ff222f8f3f931f
111443 <frank.pimenta@swisslife-select.ch>
2019-03-13 15:54:53 +0100

    . Hash#method_missing via symbols.

--- lib/application_store/core_ext/hash.rb
+++ lib/application_store/core_ext/hash.rb
@@ -1,6 +1,6 @@
 class Hash
   # TODO: try to put it working with the Hash refinement.
   def method_missing(m, *args, &blk)
-    fetch(m) { fetch(m.to_s) { super } }
+    fetch(m) { fetch(m.to_sym) { super } }
   end
 end
