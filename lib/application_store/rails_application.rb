bcb46a10d9e7a5ad3bc6acff520ecb2804b69862
111443 <frank.pimenta@swisslife-select.ch>
2019-03-23 18:20:43 +0100

    % Refactor RailsApplication#method_missing to change unless for if.

--- lib/application_store/rails_application.rb
+++ lib/application_store/rails_application.rb
@@ -8,14 +8,14 @@ module ApplicationStore
     end
 
     def method_missing method, *args, &block
-      unless method.to_s.include? '='
+      if method.to_s.include? '='
+        rails_application.config.__send__ method, *args, &block
+      else
         if rails_application.config.respond_to?(method)
           rails_application.config.__send__ method, *args, &block
         else
           rails_application.config_for(method)
         end
-      else
-        rails_application.config.__send__ method, *args, &block
       end
     end
   end
