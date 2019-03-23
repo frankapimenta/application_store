57fa786a9b5db66f1e098735ff54094d1c7384ff
111443 <frank.pimenta@swisslife-select.ch>
2019-03-23 18:21:11 +0100

    . Bump new version v0.10.0.

--- lib/application_store/version.rb
+++ lib/application_store/version.rb
@@ -1,3 +1,3 @@
 module ApplicationStore
-  VERSION = "0.9.2"
+  VERSION = "0.10.0"
 end
--- spec/application_store/version_spec.rb
+++ spec/application_store/version_spec.rb
@@ -1,3 +1,3 @@
 RSpec.describe ApplicationStore::VERSION do
-  specify { is_expected.to eq "0.9.2" }
+  specify { is_expected.to eq "0.10.0" }
 end
