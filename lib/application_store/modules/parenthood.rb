60790bacb8885185aa77dc1273d4e15f8a1b2b76
111443 <frank.pimenta@swisslife-select.ch>
2019-03-23 16:14:09 +0100

    + Add comments.

--- lib/application_store/modules/parenthood.rb
+++ lib/application_store/modules/parenthood.rb
@@ -4,6 +4,8 @@ module ApplicationStore
       @parent
     end
     def parent= parent
+      # if not StoreComposite do not set parent
+      # so no need for store composite because store are already composites when they can be parent or child
       parent.add self unless parent.nil? # if not StoreComposite will just skip due to #method_missing
       @parent = parent
     end
